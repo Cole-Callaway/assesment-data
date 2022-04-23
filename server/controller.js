0ea06bb150006f7464b1f02d7eb5a7b6ba717506
Cole Callaway <nccallaway54@gmail.com>
2022-04-22 21:16:45 -0600

    get countries

--- server/controller.js
+++ server/controller.js
@@ -13,6 +13,13 @@ const sequelize = new Sequelize(CONNECTION_STRING, {
 });
 
 module.exports = {
+  getCountries: (req, res) => {
+    sequelize
+      .query(`select * from countries`)
+      .then((dbRes) => res.status(200).send(dbRes[0]))
+      .catch((err) => console.log(err));
+  },
+
   seed: (req, res) => {
     sequelize
       .query(
