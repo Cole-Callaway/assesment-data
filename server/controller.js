b85e147d19be61db7b40b21eb4a1484622eb8728
Cole Callaway <nccallaway54@gmail.com>
2022-04-22 19:49:27 -0600

    step 3

--- server/controller.js
+++ server/controller.js
@@ -25,7 +25,12 @@ module.exports = {
                 name varchar
             );
 
-            *****YOUR CODE HERE*****
+            create table cities (
+                city_id serial primary key,
+                name varchar,
+                rating int,
+                country_id int references countries(country_id),
+            );
 
             insert into countries (name)
             values ('Afghanistan'),
